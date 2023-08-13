395c0158b8944766c3a04f7df0fe24e95e29079b
DamlaOksel <damlaoksel@hotmail.com>
2023-08-13 15:54:27 +0300

    Ana sayfa ve UserLogin&AdminLogin calisiyor.

---
+++ Scripts/admin-login.js
@@ -0,0 +1,48 @@
+const ADMIN_FORM = document.querySelector("#admin-form");
+const ADMIN_NAME = "admin";
+const ADMIN_PASS = "1234";
+
+EventListeners();
+
+function EventListeners() {
+    ADMIN_FORM.addEventListener("submit", AdminForm);
+}
+function AdminForm(e) {
+    e.preventDefault();
+
+    let adminName = ADMIN_FORM.querySelector("#admin-name");
+    let adminPass = ADMIN_FORM.querySelector("#admin-password");
+    let trimmedAdminName = adminName.value.trim();
+    let trimmedAdminPass = adminPass.value.trim();
+
+    adminName.value = "";
+    adminPass.value = "";
+
+    AdminData(trimmedAdminName, trimmedAdminPass);
+    
+    function AdminData(name, pass) {
+        if (name === ADMIN_NAME && pass === ADMIN_PASS) {
+            window.location.href = "./MainMenu.html"; //Admin paneline yönlendirilecek.
+        }
+        else if (name == "" && pass == "") {
+            DangerToast("null value");
+        }
+        else {
+            DangerToast("wrong value");
+        }
+    }
+}
+function DangerToast(text) {
+    Toastify({
+        text: `${text}`,
+        duration: 2000,
+        gravity: "top",
+        position: "right",
+        style: {
+            background: "white",
+            color: "black",
+        },
+        stopOnFocus: true,
+    }).showToast();
+}
+
---
+++ Scripts/user-login.js
@@ -0,0 +1,45 @@
+const USER_FORM = document.querySelector("#user-form");
+const USER_NAME = "user";
+const USER_PASS = "1234";
+EventListeners();
+
+function EventListeners() {
+    USER_FORM.addEventListener("submit", UserForm);
+}
+function UserForm(e) {
+    e.preventDefault();
+    let userName = USER_FORM.querySelector("#user-name");
+    let userPass = USER_FORM.querySelector("#user-password");
+    let trimmedUserName = userName.value.trim();
+    let trimmedUserPass = userPass.value.trim();
+
+    userName.value = "";
+    userPass.value = "";
+
+    UserData(trimmedUserName, trimmedUserPass);
+
+    function UserData(name, pass) {
+        if (name === USER_NAME && pass === USER_PASS) {
+            window.location.href = "./MainMenu.html"; //User paneline yönlendirilecek.
+        }
+        else if (name == "" && pass == "") {
+            DangerToast("null value");
+        }
+        else {
+            DangerToast("wrong value");
+        }
+    }
+}
+function DangerToast(text) {
+    Toastify({
+        text: `${text}`,
+        duration: 2000,
+        gravity: "top",
+        position: "right",
+        style: {
+            background: "white",
+            color: "black",
+        },
+        stopOnFocus: true,
+    }).showToast();
+}
